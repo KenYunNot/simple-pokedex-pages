4ebff4dabd56fd59a2d4d4197151c1f706abcc6e
Ken Yun <yunhaen@gmail.com>
2024-02-21 15:03:06 -0500

    Updated imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import NavLayout from "@/ui/layouts/nav-layout";
+import NavLayout from "@/lib/ui/layouts/nav-layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
