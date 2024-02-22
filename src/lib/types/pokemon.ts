ab129da6e8e9e7cb77ea29dc692c9e56d0ba31e8
Ken Yun <yunhaen@gmail.com>
2024-02-22 16:28:19 -0500

    Added Pokemon type

--- src/lib/types/pokemon.ts
+++ src/lib/types/pokemon.ts
@@ -1,9 +1,31 @@
 import { Type } from "@prisma/client"
 
+// In all use cases, if the fields are null the DexPointer itself will be represented as null.
+// This is only to satisfy the Typescript conditions
+export type DexPointer = {
+  name: string | null,
+  full_name: string | null,
+};
+
 export type PokemonBasic = {
   id: number,
   name: string,
   full_name: string,
   image_url: string,
   types: Type[],
+}
+
+export type Pokemon = {
+  id: number,
+  name: string,
+  abilities: string[],
+  image_url: string,
+  height: number,
+  weight: number,
+  genus: string,
+  flavor_texts: string[],
+  gender_rate: number,
+  left: DexPointer | null,
+  right: DexPointer | null,
+  is_default: boolean,
 }
