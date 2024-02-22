b2fdf53f79c0ff8fb92b57cb12831f5e19862ede
Ken Yun <yunhaen@gmail.com>
2024-02-22 15:30:43 -0500

    Individual card component for Pokemon

--- src/lib/ui/components/pokemon-card.tsx
+++ src/lib/ui/components/pokemon-card.tsx
@@ -1,17 +1,31 @@
 import Image from "next/image";
+import { Fragment } from "react";
 
 import { PokemonBasic } from "@/lib/types/pokemon";
+import TypeIcon from "./type-icon";
 
-export default function PokemonCard({ pokemon } : { pokemon: PokemonBasic }) {
+export default function PokemonCard({ pokemon }: { pokemon: PokemonBasic }) {
   return (
-    <div className="flex flex-col justify-center">
-      <h2>{pokemon.name}</h2>
+    <div className="relative flex flex-col border-2 p-3 rounded-md">
+      <h2 className="w-full text-xl font-semibold">{pokemon.full_name}</h2>
+      <span className="absolute top-3 right-3 w-fit px-2 bg-red-500 rounded-full text-xl font-semibold text-white">
+        {pokemon.id}
+      </span>
       <Image
         src={pokemon.image_url}
         width={400}
         height={400}
         alt={`Image of Pokemon ${pokemon.id}`}
       />
+      <span className="flex justify-center gap-1">
+        {pokemon.types.map(type => {
+          return (
+            <Fragment key={type.id}>
+              <TypeIcon name={type.name} />
+            </Fragment>
+          )
+        })}
+      </span>
     </div>
-  )
+  );
 }
