6e6a9a296f9aaacea7cc2a7ead33bc8585dab00b
Ken Yun <yunhaen@gmail.com>
2024-02-24 16:59:36 -0500

    Created separate component for Pokemon type defenses

---
+++ src/lib/ui/components/type-defenses-table.tsx
@@ -0,0 +1,14 @@
+import { Type } from "@prisma/client"
+
+
+type TypeDefenses = {
+  
+}
+
+export default function TypeDefensesTable({ types } : { types : Type[] }) {
+  return (
+    <div>
+
+    </div>
+  )
+}
