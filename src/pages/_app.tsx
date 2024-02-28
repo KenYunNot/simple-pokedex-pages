66b1351bbe89cc24b76117ff6e7750e7419d2be7
Ken Yun <yunhaen@gmail.com>
2024-02-28 13:06:30 -0500

    Refactored spacing of imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+
 import NavLayout from "@/lib/ui/layouts/nav-layout";
+
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
