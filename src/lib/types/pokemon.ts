214f65849d477f66ccfd2d8ce444824cc711f162
Ken Yun <yunhaen@gmail.com>
2024-02-28 12:38:31 -0500

    Imported type Type from prisma client

--- src/lib/types/pokemon.ts
+++ src/lib/types/pokemon.ts
@@ -1,13 +1,5 @@
-import { Type } from "@prisma/client"
-
-export type TypeWithRelations = {
-  double_damage_from: Type[],
-  double_damage_to: Type[],
-  half_damage_from: Type[],
-  half_damage_to: Type[],
-  no_damage_from: Type[],
-  no_damage_to: Type[],
-} & Type;
+import type { Type } from "@prisma/client";
+import type { TypeWithRelations } from "./type";
 
 export type AdjacentPokemon = {
   id: number,
