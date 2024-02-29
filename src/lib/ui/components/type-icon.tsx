cac328b26b1399f3ed47c460b7462cbb82f67e48
Ken Yun <yunhaen@gmail.com>
2024-02-29 14:25:46 -0500

    Background color using extended tailwind colors

--- src/lib/ui/components/type-icon.tsx
+++ src/lib/ui/components/type-icon.tsx
@@ -5,11 +5,31 @@ import clsx from "clsx";
 
 export default function TypeIcon({ name, shortened, link } : { name: string, shortened?: boolean, link?: boolean }) {
   const iconStyle = clsx(
-    `${name} flex justify-center items-center rounded border border-black border-opacity-20 text-[11px] text-white font-semibold text-shadow\
+    `flex justify-center items-center rounded border border-black border-opacity-20 text-[11px] text-white font-semibold text-shadow\
       hover:brightness-90`,
     {
       "w-[72px] h-7" : !shortened,
       "w-7 h-7 md:w-10 md:h-10 md:text-sm" : shortened,
+    },
+    {
+      "bg-bug" : name === 'bug',
+      "bg-dark" : name === 'dark',
+      "bg-dragon" : name === 'dragon',
+      "bg-electric" : name === 'electric',
+      "bg-fairy" : name === 'fairy',
+      "bg-fighting" : name === 'fighting',
+      "bg-fire" : name === 'fire',
+      "bg-flying" : name === 'flying',
+      "bg-ghost" : name === 'ghost',
+      "bg-grass" : name === 'grass',
+      "bg-ground" : name === 'ground',
+      "bg-ice" : name === 'ice',
+      "bg-normal" : name === 'normal',
+      "bg-poison" : name === 'poison',
+      "bg-psychic" : name === 'psychic',
+      "bg-rock" : name === 'rock',
+      "bg-steel" : name === 'steel',
+      "bg-water" : name === 'water',
     }
   );
 
