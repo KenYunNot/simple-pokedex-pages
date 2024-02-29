2e8bf605e4d9495ed01ea1a0c4363615076f6db3
Ken Yun <yunhaen@gmail.com>
2024-02-28 23:17:07 -0500

    Navbar is responsive for multiple screen sizes

--- src/lib/ui/components/navbar.tsx
+++ src/lib/ui/components/navbar.tsx
@@ -1,15 +1,135 @@
 import Image from "next/image"
+import Link from "next/link";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+
+import { Bars3Icon } from "@heroicons/react/24/solid";
+
+
+const links = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "Pokédex",
+    href: "/pokedex",
+  },
+  {
+    name: "Types",
+    href: "/types",
+  },
+];
 
 export default function Navbar() {
   return (
-    <nav className="w-full bg-red-500">
-      <Image 
-        src='/logo.png'
-        width={300}
-        height={77}
-        alt="Simple Pokedex logo"
-        priority={true}
-      />
-    </nav>
+    <>
+      <FullNavbar className="hidden md:flex" />
+      <MobileNavbar className="md:hidden" />
+    </>
   )
+}
+
+function FullNavbar({
+  className = "",
+}: {
+  className?: string;
+}) {
+  const pathname = usePathname();
+
+  return (
+    <nav className={`flex justify-evenly w-full px-3 bg-red-500 ${className}`}>
+      <Link href="/">
+        <Image
+          src="/logo.png"
+          width={300}
+          height={167}
+          alt="Simple Pokedex v3 logo"
+        />
+      </Link>
+      <ul className="flex justify-center items-center">
+        {links.map((link) => {
+          let isActive: boolean;
+          if (link.href === "/") {
+            isActive = pathname === link.href;
+          } else {
+            isActive = pathname.startsWith(link.href);
+          }
+          return (
+            <li key={link.href} className="w-24 h-full">
+              <Link
+                href={link.href}
+                className={clsx(
+                  "flex flex-col justify-center w-full h-full items-center bg-red-500 rounded-md text-lg text-white font-semibold",
+                  {
+                    "bg-yellow-400 pointer-events-none": isActive,
+                    "hover:bg-red-400": !isActive,
+                  }
+                )}
+              >
+                {link.name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
+
+function MobileNavbar({ className="" } : { className: string }) {
+  const [showNav, setShowNav] = useState(false);
+  const pathname = usePathname();
+
+  const listStyle = clsx(
+    "absolute top-14 left-0 w-full z-10", 
+    {
+      "hidden": !showNav,
+      "block": showNav,
+    }
+  );
+
+  return (
+    <nav className={`flex justify-between items-center h-14 px-2 bg-red-500 ${className}`}>
+      <Link href="/">
+        <Image
+          src="/logo.png"
+          width={200}
+          height={65}
+          alt="Simple Pokedex v3 logo"
+        />
+      </Link>
+      <button onClick={() => setShowNav(!showNav)}>
+        <Bars3Icon className="w-7 h-7 text-white" />
+      </button>
+      <ul className={listStyle}>
+        {links.map((link) => {
+          let isActive: boolean;
+          if (link.href === "/") {
+            isActive = pathname === link.href;
+          } else {
+            isActive = pathname.startsWith(link.href);
+          }
+          return (
+            <li key={link.name} className="w-full h-14">
+              <Link
+                href={link.href}
+                onClick={() => setShowNav(false)}
+                className={clsx(
+                  "flex justify-center items-center w-full h-full bg-red-500 text-xl text-white font-semibold",
+                  {
+                    "bg-yellow-400 pointer-events-none": isActive,
+                    "hover:bg-red-400": !isActive,
+                  }
+                )}
+              >
+                {link.name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 }
